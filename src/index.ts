1d19705808d47b280005279f683cb209839d0a2d
itsPeetah <pietro.g.moroni@gmail.com>
2023-11-17 15:40:31 +0100

    Update v1.1

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
-import express from "express"
-import routes from "./routes"
-import apiRoutes from "./routes/api"
-import getLocalAddress from "./lib/networking/getLocalAddress"
-import getFreePort from "./lib/networking/getFreePort"
-import { setGlobal } from "./lib/context/store"
-import { GLOBAL_DLDIR_KEY, GLOBAL_HOST_KEY, GLOBAL_PORT_KEY } from "./lib/context/keys"
-import download from "downloads-folder"
-import { exec } from "child_process"
+#! /usr/bin/env node
 
-const app = express()
-app.use(express.json())
-app.use(routes)
-app.use("/api", apiRoutes)
+import express from "express";
+import routes from "./routes";
+import apiRoutes from "./routes/api";
+import getLocalAddress from "./lib/networking/getLocalAddress";
+import getFreePort from "./lib/networking/getFreePort";
+import { setGlobal } from "./lib/context/store";
+import {
+  GLOBAL_DLDIR_KEY,
+  GLOBAL_HOST_KEY,
+  GLOBAL_PORT_KEY,
+} from "./lib/context/keys";
+import download from "downloads-folder";
+import { exec } from "child_process";
 
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use("/api", apiRoutes);
 
 getFreePort({ startPort: 5000 }).then((port) => {
-    const address = getLocalAddress()
+  const address = getLocalAddress();
 
-    const dls = download()
-    setGlobal(GLOBAL_DLDIR_KEY, dls)
-    setGlobal(GLOBAL_HOST_KEY, address)
-    setGlobal(GLOBAL_PORT_KEY, port)
+  const dls = download();
+  setGlobal(GLOBAL_DLDIR_KEY, dls);
+  setGlobal(GLOBAL_HOST_KEY, address);
+  setGlobal(GLOBAL_PORT_KEY, port);
 
-    app.listen(port, () => {
-        const addr = `http://${address}:${port}`
-        console.log(`Server started @ ${addr}`);
-        var start = (process.platform == 'darwin' ? 'open' : process.platform == 'win32' ? 'start' : 'xdg-open');
-        exec(`${start} ${addr}`)
-    })
-})
+  app.listen(port, () => {
+    const addr = `http://${address}:${port}`;
+    console.log(`Server started @ ${addr}`);
+    var start =
+      process.platform == "darwin"
+        ? "open"
+        : process.platform == "win32"
+        ? "start"
+        : "xdg-open";
+    exec(`${start} ${addr}`);
+  });
+});
--- src/routes/api/upload.ts
+++ src/routes/api/upload.ts
@@ -4,24 +4,23 @@ import multer from "multer";
 import getTransferFilename from "../../lib/file/getTransferFilename";
 import path from "path";
 
-const storage = multer.diskStorage(
-    {
-        destination: path.join(download(), "/FakeDrop"),
-        filename: function (_req, file, cb) {
-            cb(null, getTransferFilename(file.originalname));
-        }
-    }
-);
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, path.join(download(), "/FakeDrop"));
+  },
+
+  filename: function (_req, file, cb) {
+    cb(null, getTransferFilename(file.originalname));
+  },
+});
 
 const upload = multer({ storage });
-export const uploadMiddleware = upload.single('file');
+export const uploadMiddleware = upload.single("file");
 
 const uploadHandler: RequestHandler = (req, res, _next) => {
+  console.log("Received file:", req.file?.filename);
+  //   console.log(req.file);
+  res.status(200);
+};
 
-    console.log(req.body);
-    console.log(req.file);
-
-    res.status(200)
-}
-
-export default uploadHandler
+export default uploadHandler;
