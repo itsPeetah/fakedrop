ad1b265614a17f872c4e4eb6632f9f763dd02331
Pietro Moroni <pietro.g.moroni@gmail.com>
2024-11-03 15:50:50 +0100

    Update init.ts

--- src/routes/api/init.ts
+++ src/routes/api/init.ts
@@ -1,14 +1,13 @@
 import { RequestHandler } from 'express';
 import { getGlobal } from "../../lib/context/store";
 import { GLOBAL_HOST_KEY, GLOBAL_PORT_KEY } from '../../lib/context/keys';
-import path from 'path';
 import { CLIENT_LANDING } from '../../lib/constants';
 
 const initHandler: RequestHandler = (_req, res) => {
 
     const address = getGlobal(GLOBAL_HOST_KEY)
     const port = getGlobal(GLOBAL_PORT_KEY)
-    const client = path.join(`http://${address}:${port}`, CLIENT_LANDING)
+    const client =`http://${address}:${port}${ CLIENT_LANDING}`
 
     return res.json({ address, port, client });
 
